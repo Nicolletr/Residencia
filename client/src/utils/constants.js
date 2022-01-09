d99d8c80798392dbc4a342d2bdab440e8a270608
Nicolletr <78945498+Nicolletr@users.noreply.github.com>
2022-01-09 20:09:43 +0000

    Add files via upload

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+//export const API_URL = "https://ecommerce-strapi-vue.herokuapp.com";
+export const API_URL = "http://20.114.199.2:1337";
+export const TOKEN = "token";
+export const PRODUCTS = "products";
