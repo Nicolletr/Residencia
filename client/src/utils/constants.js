0639964a2a09d04dc8a9eaa4ef2908f526aa8811
Nicolletr <78945498+Nicolletr@users.noreply.github.com>
2022-01-09 16:15:38 -0500

    Update constants.js

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,3 @@
-//export const API_URL = "https://ecommerce-strapi-vue.herokuapp.com";
 export const API_URL = "http://20.114.199.2:1337";
 export const TOKEN = "token";
 export const PRODUCTS = "products";
