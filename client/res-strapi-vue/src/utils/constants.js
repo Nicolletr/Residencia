0c52c75198c042423ca0c6e74ed20075633d6fa4
Nicolletr <78945498+Nicolletr@users.noreply.github.com>
2022-01-11 21:01:22 -0500

    Delete client/res-strapi-vue directory

--- client/res-strapi-vue/src/utils/constants.js
+++
@@ -1,4 +0,0 @@
-//export const API_URL = "https://ecommerce-strapi-vue.herokuapp.com";
-export const API_URL = "http://localhost:1337";
-export const TOKEN = "token";
-export const PRODUCTS = "products";
